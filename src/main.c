a66b195108585cdcae4ff8b6f8e1c5d5f0c329bc
Roz Wyatt-Millington <r.wyatt-millington@leedsbeckett.ac.uk>
2025-07-10 13:33:36 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -46,8 +46,6 @@ void chip_init() {
   chip->pin_qbar = pin_init("Q_bar", OUTPUT_HIGH);
   chip->val_q = LOW;
   chip->val_qbar = HIGH;
-  pin_write(chip->pin_q, chip->val_q);
-  pin_write(chip->pin_qbar, chip->val_qbar);
   const pin_watch_config_t watch_config = {
     .edge = RISING,
     .pin_change = chip_pin_change,
